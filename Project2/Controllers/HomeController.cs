1319c9f795eb911ffba935bbdabedd249735203f
tyscurtis25 <tyscurtis@gmail.com>
2022-03-22 19:48:29 -0600

    Adding necessary IActions to the home controller

--- Project2/Controllers/HomeController.cs
+++ Project2/Controllers/HomeController.cs
@@ -21,53 +21,5 @@ namespace Project2.Controllers
             return View();
         }
 
-        //public IActionResult TaskView()
-        //{
-        //    var Tasks = DbContext.Tasks
-        //        .Where(x => x.Completed == false)
-        //        .ToList();
-
-        //    return View(Tasks);
-        //}
-
-        //[HttpGet]
-        //public IActionResult Edit(int taskid)
-        //{
-        //    ViewBag.Categories = DbContext.Categories.ToList();
-
-        //    var tasks = DbContext.Tasks.Single(x => x.TaskId == taskid);
-
-        //    return View("TaskForm", tasks);
-        //}
-
-        //[HttpPost]
-        //public IActionResult Edit(Tasks ta)
-        //{
-        //    DbContext.Update(ta);
-        //    DbContext.SaveChanges();
-        //    return RedirectToAction("TaskView");
-        //}
-
-        //[HttpGet]
-        //public IActionResult Delete(int taskid)
-        //{
-        //    var tasks = DbContext.Tasks.Single(x => x.TaskId == taskid);
-        //    return View(tasks);
-        //}
-
-        //[HttpPost]
-        //public IActionResult Delete(Tasks ta)
-        //{
-        //    DbContext.Tasks.Remove(ta);
-        //    DbContext.SaveChanges();
-
-        //    return RedirectToAction("TaskView");
-        //}
-
-        //public IActionResult Confirmation()
-        //{
-        //    return View();
-        //}
-
     }
 }
